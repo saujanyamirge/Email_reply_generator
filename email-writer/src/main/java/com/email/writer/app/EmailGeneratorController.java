9cc64e272f2d9b6038dd517f399dd3bdff67b104
saujanyamirge <126487192+saujanyamirge@users.noreply.github.com>
2025-06-11 18:26:29 +0530

    Add files via upload

---
+++ email-writer/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -0,0 +1,22 @@
+package com.email.writer.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
